db64cb16127aa40e9f1f65ea92e84baac02371e4
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2018-12-02 00:37:37 +0100

    Refactor method

--- app/githubStats/GithubStatsService.scala
+++ app/githubStats/GithubStatsService.scala
@@ -9,23 +9,26 @@ import scala.concurrent.Future
 
 class GithubStatsService @Inject()(httpClient: HttpClient) {
 
-  def getTopComittersOfRepo(owner: String, repository: String): Future[List[GithubCommitter]] = {
-    httpClient.get(GithubApiRoutes.repositoryCommits(owner, repository))
-      .map(rawToSortedGithubComitters)
+  def getTopComittersOfRepo(owner: String, repositoryName: String): Future[List[GithubCommitter]] = {
+    getLastCommitsOfRepository(owner, repositoryName)
+      .map(commits => {
+        commits.groupBy(_.authorEmail)
+          .map(groupedCommits => {
+            val commits = groupedCommits._2
+            val firstCommit = commits.head
+            GithubCommitter(firstCommit.authorName, firstCommit.authorEmail, commits.size)
+          })
+          .toStream
+          .sortBy(_.commits)(Ordering[Int].reverse)
+          .toList
+          .slice(0, 10)
+      })
+
   }
 
-  private def rawToSortedGithubComitters(rawResponse: JsValue): List[GithubCommitter] = {
-    EntitiesMapping.jsonToGithubCommitArray(rawResponse.as[JsArray])
-      .groupBy(_.authorEmail)
-      .map(groupedCommits => {
-        val commits = groupedCommits._2
-        val firstCommit = commits.head
-        GithubCommitter(firstCommit.authorName, firstCommit.authorEmail, commits.size)
-      })
-      .toStream
-      .sortBy(_.commits)(Ordering[Int].reverse)
-      .toList
-      .slice(0, 10)
+  private def getLastCommitsOfRepository(owner: String, repositoryName: String): Future[List[GithubCommit]] = {
+    httpClient.get(GithubApiRoutes.repositoryCommits(owner, repositoryName))
+      .map(rawResponse => EntitiesMapping.jsonToGithubCommitArray(rawResponse.as[JsArray]))
   }
 
   def getTopLanguagesOfUser(username: String): Future[List[LanguageUsage]] = {
@@ -49,14 +52,14 @@ class GithubStatsService @Inject()(httpClient: HttpClient) {
       })
   }
 
-  def getLanguagesOfRepository(githubRepository: GithubRepository): Future[List[LanguageUsage]] = {
+  private def getLanguagesOfRepository(githubRepository: GithubRepository): Future[List[LanguageUsage]] = {
     httpClient.get(GithubApiRoutes.languagesOfRepository(githubRepository.owner, githubRepository.name))
       .map(raw => {
         EntitiesMapping.rawToLanguageUsage(raw.as[JsObject])
       })
   }
 
-  def getRepositoriesOfUser(username: String): Future[List[GithubRepository]] = {
+  private def getRepositoriesOfUser(username: String): Future[List[GithubRepository]] = {
     httpClient.get(GithubApiRoutes.repositoriesOfUser(username))
       .map(raw => {
         EntitiesMapping.rawToGithubRepositories(raw.as[JsArray])
