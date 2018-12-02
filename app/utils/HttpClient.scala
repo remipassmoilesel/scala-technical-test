c980c179c56a0da362880e3de68a42726f5e97b2
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2018-12-02 15:11:30 +0100

    Add more detailled errors on http client

--- app/utils/HttpClient.scala
+++ app/utils/HttpClient.scala
@@ -18,17 +18,24 @@ class HttpRequestException(message: String) extends Exception(message) {
 
 class HttpClientImpl @Inject()(wsClient: WSClient) extends HttpClient {
 
-  private def checkStatus(response: WSResponse, expectedStatus: Int): Unit = {
-    if (response.status != expectedStatus) {
-      throw new HttpRequestException(s"Bad status code: ${response.status} ($expectedStatus expected)")
-    }
-  }
-
   def get(url: String, expectedStatus: Int = 200): Future[JsValue] = {
     getRequest(url).get().map(response => {
       checkStatus(response, expectedStatus)
       response.json
     })
+  }
+
+  private def checkStatus(response: WSResponse, expectedStatus: Int): Unit = {
+    if (response.status != expectedStatus) {
+      throw new HttpRequestException(
+        s"""
+        Bad status code: ${response.status} ($expectedStatus expected)
+
+        ResponseBody=${response.json}
+
+        ResponseHeaders=${response.headers}
+        """)
+    }
   }
 
   private def getRequest(url: String): WSRequest = {
